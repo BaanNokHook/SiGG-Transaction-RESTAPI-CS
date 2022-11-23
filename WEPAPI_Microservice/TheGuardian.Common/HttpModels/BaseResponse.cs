d1fd1384a541c303049d202e7d89bf31f6ec71f6
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-23 15:58:01 +0700

    SiGG-Transaction-RESTAPI-CS-WEPAPI_Microservice-TheGuarDian

---
+++ WEPAPI_Microservice/TheGuardian.Common/HttpModels/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Michaelsoft.BodyGuard.Common.HttpModels
+{
+    public class BaseResponse
+    {
+
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; }
+
+    }
+}
---
+++ WEPAPI_Microservice/WEPAPI_Microservice/Controllers/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AuthenticationController.Controllers
+{
+    internal class UserService
+    {
+        internal object GetByEmail(object emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UpdatePassword(object id, object newPassword, object newPasswordConfirm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
