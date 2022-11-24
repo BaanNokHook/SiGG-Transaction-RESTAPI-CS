6fe61d6824f6db8a2019445d55e9e2205e997d50
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-24 11:18:38 +0700

    SiGG-Transaction-RESTAPI-CS-WEPAPI_Microservice-TheGuarDian-DatabaseModels-DbToken

---
+++ WEPAPI_Microservice/WEPAPI_Microservice/DatabaseModels/DbToken.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace WEPAPI_Microservice.DatabaseModels
+{
+    public class DbToken
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_userId")]
+        public string UserId { get; set; } 
+        public string Type { get; set; }
+        public string Value { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]  
+        public DateTime Created { get; set; }
+
+        [BsonElement("expiredAt")]
+        [JsonIgnore]  
+        public DateTime ExpireAt { get; set; }   
+
+    }
+}
+
+
+
