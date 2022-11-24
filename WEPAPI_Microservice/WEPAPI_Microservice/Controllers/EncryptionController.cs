a479cfd6ec2e7128f11b6799ec3cc4d6b69bf8c1
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-24 10:10:54 +0700

    SiGG-Transaction-RESTAPI-CS-WEPAPI_Microservice-TheGuarDian-Controller-EncryptionController

---
+++ WEPAPI_Microservice/WEPAPI_Microservice/Controllers/EncryptionController.cs
@@ -0,0 +1,35 @@
+﻿using AuthenticationController.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Options;
+using System.Data;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WEPAPI_Microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class EncryptionController
+    {
+        [AllowAnonymous]
+        [HttpGet("[action]")]  
+        public string GenerateAesParameters()
+        {
+            [AllowAnonymous]
+            [HttpGet("[action]")]  
+            public string GenerateAesParameters()
+            {
+                var aes = new AesManaged { KeySize = 256 };
+                aes.GenerateKey();
+                aes.GenerateIV();
+                return EncodingHelper.ToSafeUrlBase64(aes.Key) + "\n" + EncodingHelper.ToSafeUrlBase64(aes.IV);  
+            }
+        }
+    }
+}
+
+
