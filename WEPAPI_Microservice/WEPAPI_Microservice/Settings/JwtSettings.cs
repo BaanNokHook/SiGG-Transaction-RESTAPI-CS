f011b9491eb038b0d2999d02b896cfc906115f6e
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-25 09:29:37 +0700

    SiGG-Transaction-RESTAPI-CS-WEPAPI_Microservice-TheGuarDian-Settings-JwtSettings

---
+++ WEPAPI_Microservice/WEPAPI_Microservice/Settings/JwtSettings.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace WEPAPI_Microservice.Settings
+{
+
+    [JsonObject("JwtSettings")]  
+
+    public class JwtSettings
+    {
+
+        [JsonProperty("Secret")]  
+        public string Secret { get; set; }
+
+
+        [JsonProperty("Issuer")]  
+        public string Issuer { get; set; }
+
+
+        [JsonProperty("Audience")]
+        public string Audience { get; set; }
+
+
+        [JsonProperty("AccessException")]
+        public int AccessExpiration { get; set; }
+
+
+        [JsonProperty("RefreshExpiration")]
+
+        public int RefreshExpiration { get; set; }
+
+    
+    }
+
+    internal class JsonObjectAttribute : Attribute
+    {
+        private string v;
+
+        public JsonObjectAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+
+    internal class JsonPropertyAttribute : Attribute
+    {
+        private string v;
+
+        public JsonPropertyAttribute(string v)
+        {
+            this.v = v;
+        }
+    }
+}
+
+
