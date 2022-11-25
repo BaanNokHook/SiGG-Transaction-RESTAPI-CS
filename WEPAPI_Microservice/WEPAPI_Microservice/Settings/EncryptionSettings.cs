44197f222e224127c0c9786428e8ac7e395cbb56
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-25 09:12:38 +0700

    SiGG-Transaction-RESTAPI-CS-WEPAPI_Microservice-TheGuarDian-Settings-EncryptionSettings

---
+++ WEPAPI_Microservice/WEPAPI_Microservice/Settings/EncryptionSettings.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEPAPI_Microservice.Settings
+{
+
+    public class EncryptionSettings : IEncryptionSettings
+    {
+
+        public string DataEncryptionKey { get; set; }  
+
+        public string DataEncryptionIv { get; set; }
+
+    }
+
+    public interface IEncryptionSettings
+    {
+
+        public string DataEncryptionKey { get; set; }  
+        
+
+        public string DataEncryptionIv { get; set; }
+
+    }
+
+}
+
