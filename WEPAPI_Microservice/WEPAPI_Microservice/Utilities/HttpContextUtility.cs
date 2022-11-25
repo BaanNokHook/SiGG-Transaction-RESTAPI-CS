f2774ed12aaf1ba5d50123664a70a9b4773d3427
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-25 09:55:28 +0700

    SiGG-Transaction-RESTAPI-CS-WEPAPI_Microservice-TheGuarDian-Utilities-HttpContextUtility

---
+++ WEPAPI_Microservice/WEPAPI_Microservice/Utilities/HttpContextUtility.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WEPAPI_Microservice.Utilities
+{
+    public class HttpContextUtility : InjectedHttpContestBaseStaticClass
+    {
+
+        public static string LoggedUserIdentityId()
+        {
+            if (!(HttpContext.User.Identity is ClaimsIdentity identity) || !identity.IsAuthenticated) return null;
+            return (from claim in identity.Claims
+                    where new[] { "sub", ClaimTypes.NameIdentifier }.Contains(claim.Type)
+                    select claim.Value).FirstOrDefault();    
+        }
+    }
+
+    public class InjectedHttpContestBaseStaticClass
+    {
+    }
+}
+
+
